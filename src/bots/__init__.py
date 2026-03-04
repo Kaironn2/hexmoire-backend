4f5f718a8a6b83e06de9ca09f476a75a3085bb95
kaironn <jonathas_oliveira17@hotmail.com>
2026-03-04 15:04:24 -0300

    remove: legacy browser imports

--- src/bots/__init__.py
+++ src/bots/__init__.py
@@ -1,4 +1,3 @@
 from src.infra.http import HttpClient, HttpClientConfig
-from src.infra.browser import BrowserClient, BrowserConfig
 
-__all__ = ['HttpClient', 'HttpClientConfig', 'BrowserClient', 'BrowserConfig']
+__all__ = ['HttpClient', 'HttpClientConfig']
